7574a02db9487ca02b15d17d01066c5d5ea8f83c
Johannes Ranke <johannes.ranke@jrwb.de>
2024-12-06 23:32:26 +0100

    Add some more plugins to the lua config
    The plugins still need to be configured

---
+++ nvim/lua/plugins/misc.lua
@@ -0,0 +1,30 @@
+return {
+  -- airline
+  { "vim-airline/vim-airline" },
+  { "vim-airline/vim-airline-themes" },
+
+  -- pandoc
+  { "vim-pandoc/vim-pandoc" },
+  { "vim-pandoc/vim-pandoc-syntax" },
+  
+  -- kwbdi
+  --[[ The following plugin gives me <Leader>bd for closing a buffer while keeping
+       the window open, for example when editing several R scripts with
+       the R interpreter running in a separate vim window ]]
+  { "seb-mueller/kwbdi.vim" },
+
+  -- Version control
+  { "tpope/vim-fugitive" },
+  { "airblade/vim-gitgutter" },
+  { "samoshkin/vim-mergetool" },
+
+  -- Show an manage marks
+  { "kshenoy/vim-signature" },
+
+  -- Wayland clipboard
+  { "jasonccox/vim-wayland-clipboard" },
+  
+  -- vimcmdline
+  { "jalvesaq/vimcmdline" },
+
+}
